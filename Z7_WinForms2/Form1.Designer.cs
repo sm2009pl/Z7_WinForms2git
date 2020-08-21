3df0d8f14a08737385d3e7ce91d160b9fc4377f9
Szymon Magiera <szimon.doto@hotmail.com>
2020-08-21 23:31:48 +0200

    cmt
    cmt

--- Z7_WinForms2/Form1.Designer.cs
+++ Z7_WinForms2/Form1.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.colorDialog1 = new System.Windows.Forms.ColorDialog();
@@ -39,52 +38,43 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.button4 = new System.Windows.Forms.Button();
             this.fontDialog1 = new System.Windows.Forms.FontDialog();
-            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
-            this.printDialog1 = new System.Windows.Forms.PrintDialog();
+            this.button4 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.button5 = new System.Windows.Forms.Button();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
-            this.button6 = new System.Windows.Forms.Button();
-            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(74, 93);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(222, 20);
-            this.textBox1.TabIndex = 0;
-            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(450, 91);
+            this.button1.Location = new System.Drawing.Point(401, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(190, 93);
+            this.button1.Size = new System.Drawing.Size(190, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "Zmiana czcionki";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(126, 307);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 164);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(77, 71);
+            this.pictureBox1.Size = new System.Drawing.Size(378, 274);
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(396, 229);
+            this.dateTimePicker1.Location = new System.Drawing.Point(555, 244);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(141, 20);
+            this.dateTimePicker1.Size = new System.Drawing.Size(227, 20);
             this.dateTimePicker1.TabIndex = 3;
             // 
             // monthCalendar1
             // 
-            this.monthCalendar1.Location = new System.Drawing.Point(601, 216);
+            this.monthCalendar1.Location = new System.Drawing.Point(555, 276);
             this.monthCalendar1.Name = "monthCalendar1";
             this.monthCalendar1.TabIndex = 4;
             // 
@@ -95,83 +85,70 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(631, 61);
+            this.button2.Location = new System.Drawing.Point(401, 41);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(76, 30);
+            this.button2.Size = new System.Drawing.Size(190, 23);
             this.button2.TabIndex = 5;
-            this.button2.Text = "button2";
+            this.button2.Text = "Zmiana koloru";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(690, 120);
+            this.button3.Location = new System.Drawing.Point(401, 70);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(39, 53);
+            this.button3.Size = new System.Drawing.Size(190, 23);
             this.button3.TabIndex = 6;
-            this.button3.Text = "button3";
+            this.button3.Text = "Otwórz drugie okno";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(315, 277);
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(201, 86);
+            this.richTextBox1.Size = new System.Drawing.Size(378, 135);
             this.richTextBox1.TabIndex = 7;
             this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(476, 22);
+            this.button4.Location = new System.Drawing.Point(401, 99);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(60, 21);
-            this.button4.TabIndex = 8;
-            this.button4.Text = "button4";
+            this.button4.Size = new System.Drawing.Size(190, 23);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "Zmien obraz";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
-            // printDialog1
+            // openFileDialog1
             // 
-            this.printDialog1.UseEXDialog = true;
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "bmp files (*.jpg)|*.jpg";
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(372, 29);
+            this.button5.Location = new System.Drawing.Point(401, 128);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(46, 44);
-            this.button5.TabIndex = 9;
-            this.button5.Text = "button5";
+            this.button5.Size = new System.Drawing.Size(190, 23);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "Progres pasek";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(12, 390);
+            this.progressBar1.Location = new System.Drawing.Point(401, 164);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(757, 35);
-            this.progressBar1.TabIndex = 10;
-            // 
-            // button6
-            // 
-            this.button6.Location = new System.Drawing.Point(185, 23);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(132, 56);
-            this.button6.TabIndex = 11;
-            this.button6.Text = "button6";
-            this.button6.UseVisualStyleBackColor = true;
-            // 
-            // webBrowser1
-            // 
-            this.webBrowser1.Location = new System.Drawing.Point(146, 179);
-            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
-            this.webBrowser1.Name = "webBrowser1";
-            this.webBrowser1.Size = new System.Drawing.Size(149, 69);
-            this.webBrowser1.TabIndex = 12;
+            this.progressBar1.Size = new System.Drawing.Size(381, 23);
+            this.progressBar1.TabIndex = 15;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.webBrowser1);
-            this.Controls.Add(this.button6);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
@@ -182,18 +159,14 @@
             this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.ColorDialog colorDialog1;
@@ -203,14 +176,11 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.Button button4;
         private System.Windows.Forms.FontDialog fontDialog1;
-        private System.Drawing.Printing.PrintDocument printDocument1;
-        private System.Windows.Forms.PrintDialog printDialog1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.ProgressBar progressBar1;
-        private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.WebBrowser webBrowser1;
     }
 }
 
--- Z7_WinForms2/Form1.cs
+++ Z7_WinForms2/Form1.cs
@@ -21,5 +21,50 @@ namespace Z7_WinForms2
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            fontDialog1.ShowDialog();
+            var font = fontDialog1.Font;
+            var color = fontDialog1.Color;
+            richTextBox1.Font = font;
+            richTextBox1.ForeColor = color;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.ForeColor = Color.White;
+            var result = colorDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                button2.BackColor = colorDialog1.Color;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var form2 = new Form2();
+            form2.ShowDialog();
+            richTextBox1.Text = form2.text;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = new Bitmap(openFileDialog1.FileName);
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            progressBar1.PerformStep();
+        }
     }
 }
